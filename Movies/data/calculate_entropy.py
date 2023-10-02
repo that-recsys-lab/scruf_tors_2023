8d49691e95df20938930a9ae30e095e8984f5f43
farastu-who <34352153+farastu-who@users.noreply.github.com>
2023-10-02 12:54:28 -0600

    Update calculate_entropy.py

--- Movies/data/calculate_entropy.py
+++ Movies/data/calculate_entropy.py
@@ -2,15 +2,15 @@ import numpy as np
 import pandas as pd
 from scipy.stats import entropy
 
-dataset_users = pd.read_csv('data/rating_5000_cls_hc_10core_iterative.csv', delimiter='\t', header=None)
-dataset_features = pd.read_csv('data/loan_feature_df_hc_10core.csv',header=None)
+dataset_users = pd.read_csv('ratings.csv', header=None)
+dataset_features = pd.read_csv('items.csv',header=None)
 
 # Set header names
-dataset_users.columns = ['user_id', 'loan_id', 'rating']
-dataset_features.columns = ['loan_id', 'feature_id', 'feature_value']
+dataset_users.columns = ['user_id', 'movie_id', 'rating']
+dataset_features.columns = ['movie_id', 'feature_id', 'feature_value']
 
 # Merge the two datasets on loan_id
-dataset_merged = pd.merge(dataset_users, dataset_features, on='loan_id')
+dataset_merged = pd.merge(dataset_users, dataset_features, on='movie_id')
 
 unique_features = dataset_merged['feature_id'].unique()
 
@@ -19,13 +19,13 @@ results = []
 
 for user in dataset_merged['user_id'].unique():
     user_data = dataset_merged[dataset_merged['user_id'] == user]
-    count_loans_funded = np.zeros(len(unique_features))
+    count_movies_funded = np.zeros(len(unique_features))
     
     for idx, feature in enumerate(unique_features):
-        count_loans_with_feature = len(user_data[user_data['feature_id'] == feature])
-        count_loans_funded[:count_loans_with_feature] = 1
+        count_movies_with_feature = len(user_data[user_data['feature_id'] == feature])
+        count_movies_funded[:count_movies_with_feature] = 1
         
-        entropy_val = entropy(count_loans_funded)
+        entropy_val = entropy(count_movies_funded)
         
         results.append([user, feature, entropy_val])
 
