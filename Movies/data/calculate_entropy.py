93f5ca9d3eb212a3ca70723fca40e9fb2c6aba2c
farastu-who <34352153+farastu-who@users.noreply.github.com>
2023-10-04 16:48:33 -0600

    Update calculate_entropy.py

--- Movies/data/calculate_entropy.py
+++ Movies/data/calculate_entropy.py
@@ -9,7 +9,7 @@ dataset_features = pd.read_csv('items.csv',header=None)
 dataset_users.columns = ['user_id', 'movie_id', 'rating']
 dataset_features.columns = ['movie_id', 'feature_id', 'feature_value']
 
-# Merge the two datasets on loan_id
+# Merge the two datasets on movie_id
 dataset_merged = pd.merge(dataset_users, dataset_features, on='movie_id')
 
 unique_features = dataset_merged['feature_id'].unique()
@@ -19,13 +19,13 @@ results = []
 
 for user in dataset_merged['user_id'].unique():
     user_data = dataset_merged[dataset_merged['user_id'] == user]
-    count_movies_funded = np.zeros(len(unique_features))
+    count_movies = np.zeros(len(unique_features))
     
     for idx, feature in enumerate(unique_features):
         count_movies_with_feature = len(user_data[user_data['feature_id'] == feature])
-        count_movies_funded[:count_movies_with_feature] = 1
+        count_movies[:count_movies_with_feature] = 1
         
-        entropy_val = entropy(count_movies_funded)
+        entropy_val = entropy(count_movies)
         
         results.append([user, feature, entropy_val])
 
